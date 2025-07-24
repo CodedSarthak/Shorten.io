04114696d9353a8e8f62da128e6da947b3352b66
CodedSarthak <jejurkarsarthak17@gmail.com>
2025-07-24 23:43:40 +0530

    stats controller done

--- src/controllers/url.controllers.js
+++ src/controllers/url.controllers.js
@@ -7,6 +7,8 @@ import { encodeBase62 } from '../utils/base62.js'
 
 import { client } from '../config/redisClient.js';
 
+import geoip from 'geoip-lite';
+
 const shortenURL = async (req, res) => {
     
     try 
@@ -73,6 +75,32 @@ const redirectURL = async (req, res) => {
         //cache
         await client.setEx(redisKey , 86400, urlEntry.longURL); //TTL : 1d
 
+        //Saving to stats model
+        const ip = req.headers['x-forwarded-for']?.split(',')[0] || req.socket.remoteAddress;
+        const geo = geoip.lookup(ip);
+        const countryCode = geo?.country || 'UNKNOWN';
+
+        let stats = await Stats.findOne({shortID : urlEntry._id});
+
+        if (stats)
+        {
+            stats.clicked += 1;
+            stats.lastAccessed = Date.now();
+            const existingCount = stats.geoDistribution.get(countryCode) || 0;
+            stats.geoDistribution.set(countryCode, existingCount + 1);
+        }
+        else
+        {
+            stats = new Stats ({
+                shortID: urlEntry._id,
+                clicked: 1,
+                lastAccessed: Date.now(),
+                geoDistribution: new Map([[countryCode, 1]])
+            })
+        }
+
+        await stats.save();
+
         return res.redirect(urlEntry.longURL);
 
     } 
@@ -86,6 +114,41 @@ const redirectURL = async (req, res) => {
 
 const statsURL = async (req, res) => {
 
+    const { ShortUrl } = req.params;
+
+    try 
+    {
+        const urlEntry = await Url.findOne({ shortID: ShortUrl });
+
+        if (!urlEntry) 
+        {
+            return res.status(404).json({ error: "Short URL not found" });
+        }
+
+        const stats = await Stats.findOne({shortID : urlEntry._id});
+
+        if (!stats) 
+        {
+            return res.status(404).json({ error: "No stats found" });
+        }
+
+        return res.status(200)
+                  .json({message : "Fetched Successfully", 
+                    stats: 
+                    {
+                        clicked: stats.clicked,
+                        lastAccessed: stats.lastAccessed,
+                        geoDistribution: Object.fromEntries(stats.geoDistribution), 
+                        createdAt: stats.createdAt,
+                        updatedAt: stats.updatedAt
+                    }
+                  });
+    } 
+    catch (error) 
+    {
+        console.error("Error in Stats:", error);
+        return res.status(500).json({ error: "Server error" });
+    }
 }
 
 export {
